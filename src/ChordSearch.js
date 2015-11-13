87ff4aac42a495086513aad56e0debfb755c87c3
William Chargin <wchargin@gmail.com>
2015-11-13 13:03:32 -0800

    Add the chord search core

---
+++ src/ChordSearch.js
@@ -0,0 +1,31 @@
+import * as Utils from './Utils';
+
+export default function search(a, b) {
+    if (a === b) {
+        return { tree: a, position: null };
+    }
+    if (a > b) {
+        const error = "The second input can't be larger than the first input.";
+        return { error };
+    }
+
+    const gcd = Utils.gcd(a, b);
+    const pair = findPair(a / gcd, b / gcd);
+    return {
+        tree: gcd,
+        position: pair,
+    };
+}
+
+function findPair(a, b) {
+    if (a === 1) {
+        return [ b - 1, 1 ];
+    }
+
+    const c = b % a;
+    const t = (b - c) / a;
+
+    const [ ra, rb ] = findPair(c, a);
+    const factor = Math.pow(2, t - 1);
+    return [ ra + t, (2 * rb - 1) * factor + 1 ];
+}
---
+++ test/ChordSearchSpec.js
@@ -0,0 +1,22 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+
+import search, * as ChordSearchUtils from '../src/ChordSearch';
+
+describe('ChordSearch', () => {
+    const test = (a, b, expected) => it(`works for input (${a}, ${b})`,
+        () => expect(search(a, b)).to.deep.equal(expected));
+    test(10, 22, { tree: 2, position: [ 6, 3 ] });
+    test(6, 6, { tree: 6, position: null });
+    test(6, 7, { tree: 1, position: [ 6, 2 ] });
+    test(0, 0, { tree: 0, position: null });
+    test(123, 234, { tree: 3, position: [ 14, 1028 ] });
+
+    const fail = (a, b, message) => it(`works for input (${a}, ${b})`,
+        () => {
+            const result = search(a, b);
+            expect(result).to.be.ok;
+            expect(result.error).to.contain(message);
+        });
+    fail(10, 5, "than");
+})

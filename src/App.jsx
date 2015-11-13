37eefff9b289fd1e6f806a348471d4c98bcb9c3d
William Chargin <wchargin@gmail.com>
2015-11-13 12:28:35 -0800

    Boilerplate commit with application structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
+
+export default class App extends Component {
+    render() {
+        return <div className="container">
+            <h1>Hello, app!</h1>
+        </div>;
+    }
+}
